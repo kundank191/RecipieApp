d0687230bb9a3c5fd71151c274070c7403404f20
Kundan <kundank191@gmail.com>
2018-08-12 21:53:46 +0530

    feat : implement empty state

--- app/src/main/java/com/example/kunda/bakingapp/ui/list/MainActivity.java
+++ app/src/main/java/com/example/kunda/bakingapp/ui/list/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.kunda.bakingapp.ui.list;
 
-import android.content.Intent;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.support.constraint.Group;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.kunda.bakingapp.R;
 import com.example.kunda.bakingapp.data.RecipeResponse;
-import com.example.kunda.bakingapp.ui.details.StepListActivity;
+import com.example.kunda.bakingapp.ui.RecipeViewModel;
+import com.example.kunda.bakingapp.ui.ViewModelFactory;
 import com.example.kunda.bakingapp.utils.JSONUtils;
 import com.example.kunda.bakingapp.utils.NetworkUtils;
 
@@ -21,11 +26,19 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements OnDataReceivedListener{
+public class MainActivity extends AppCompatActivity implements OnDataReceivedListener {
 
+    @BindView(R.id.no_internet_view)
+    TextView mNoInternetView;
+    @BindView(R.id.error_view)
+    TextView mErrorView;
     @BindView(R.id.recipeList_rv)
     RecyclerView mRecyclerView;
+    @BindView(R.id.progress_view)
+    Group mProgressView;
     RecipeAdapter mAdapter;
+    private RecipeViewModel mViewModel;
+    private ViewModelFactory mFactory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,36 +46,108 @@ public class MainActivity extends AppCompatActivity implements OnDataReceivedLis
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
-        requestData();
+
+        //Setting up viewModelFactory and recipeViewModel
+        mFactory = new ViewModelFactory();
+        mViewModel = ViewModelProviders.of(this, mFactory).get(RecipeViewModel.class);
+
+        if (mViewModel.getRecipeList() != null) {
+            populateUI(mViewModel.getRecipeList());
+        } else {
+            requestData();
+        }
     }
 
     /**
-     * Makes a call to the getRecipeData function which make a network call to get new data
+     * Makes a network call and gets data from internet
      */
-    private void requestData(){
-        NetworkUtils.getRecipeData(this);
+    private void requestData() {
+        if (isNetworkConnected()) {
+            showLoadingScreen();
+            NetworkUtils.getRecipeData(this);
+        } else {
+            //Complete it
+            showNoInternetUI();
+        }
     }
 
-    private void populateUI(List<RecipeResponse> recipeResponseList){
-        mAdapter = new RecipeAdapter(this,recipeResponseList);
+    private void populateUI(List<RecipeResponse> recipeResponseList) {
+
+        mProgressView.setVisibility(View.GONE);
+        mErrorView.setVisibility(View.GONE);
+        mNoInternetView.setVisibility(View.GONE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+
+        mAdapter = new RecipeAdapter(this, recipeResponseList);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setVerticalFadingEdgeEnabled(true);
         mRecyclerView.setAdapter(mAdapter);
     }
 
-    public void clicked(View view){
-        startActivity(new Intent(this, StepListActivity.class));
-    }
 
+    /**
+     * This function gets the response from the internet converts it into a list of recipe
+     * then updates the viewModel and then the UI
+     *
+     * @param response from the internet
+     */
     @Override
     public void onResponse(JSONArray response) {
         List<RecipeResponse> listRecipe = JSONUtils.getRecipeListFromJSON(response);
-        Log.d("List Recipe",listRecipe.get(0).getName());
-
-        populateUI(listRecipe);
+        Log.d("List Recipe", listRecipe.get(0).getName());
+        mViewModel.setRecipeList(listRecipe);
+        populateUI(mViewModel.getRecipeList());
     }
 
+    /**
+     * If some error is thrown during network data request
+     *
+     * @param error which was thrown
+     */
     @Override
     public void onError(String error) {
-        Log.d("Error",error);
+        Log.d("Error", error);
+        showErrorUI();
+    }
+
+    /**
+     * It checks if the device is connected to the internet or not
+     *
+     * @return a boolean value indicating the state of network
+     */
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        assert cm != null;
+        return cm.getActiveNetworkInfo() != null;
+    }
+
+    /**
+     * If an error occurs then an error layout will be shown
+     */
+    private void showErrorUI() {
+        mRecyclerView.setVisibility(View.GONE);
+        mProgressView.setVisibility(View.GONE);
+        mErrorView.setVisibility(View.VISIBLE);
+        mNoInternetView.setVisibility(View.GONE);
+    }
+
+    /**
+     * When data is being loaded then a loading layout will be shown
+     */
+    private void showLoadingScreen() {
+        mRecyclerView.setVisibility(View.GONE);
+        mProgressView.setVisibility(View.VISIBLE);
+        mErrorView.setVisibility(View.GONE);
+        mNoInternetView.setVisibility(View.GONE);
+    }
+
+    /**
+     * If there is no internet connection then a No internet layout will be shown
+     */
+    private void showNoInternetUI() {
+        mRecyclerView.setVisibility(View.GONE);
+        mProgressView.setVisibility(View.GONE);
+        mErrorView.setVisibility(View.GONE);
+        mNoInternetView.setVisibility(View.VISIBLE);
     }
 }
