0de9679a2be3fcf1716612d29c8949dd62128e2f
Kundan <kundank191@gmail.com>
2018-08-13 12:27:04 +0530

    feat : add click listener and local images for item in MainActivity

--- app/src/main/java/com/example/kunda/bakingapp/ui/list/RecipeAdapter.java
+++ app/src/main/java/com/example/kunda/bakingapp/ui/list/RecipeAdapter.java
@@ -1,15 +1,19 @@
 package com.example.kunda.bakingapp.ui.list;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.kunda.bakingapp.R;
 import com.example.kunda.bakingapp.data.RecipeResponse;
+import com.example.kunda.bakingapp.ui.details.StepListActivity;
 
 import java.util.List;
 
@@ -19,12 +23,22 @@ import butterknife.ButterKnife;
 /**
  * Created by Kundan on 08-08-2018.
  */
-public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder> {
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder>  {
 
+    private Integer[] imageID = {R.drawable.nutella_pie
+            , R.drawable.brownie
+            , R.drawable.yellow_cake
+            , R.drawable.cheese_cake};
 
     class ViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.recipeName)
         TextView recipeNameTV;
+        @BindView(R.id.recipe_image_iv)
+        ImageView recipeImageIV;
+        @BindView(R.id.servings_tv)
+        TextView servingsTV;
+        @BindView(R.id.list_item_recipe)
+        ConstraintLayout listItemRecipe;
 
         ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -39,7 +53,7 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder
      * @param context            takes the context of the activity
      * @param recipeResponseList the list of the Recipe
      */
-    public RecipeAdapter(Context context, List<RecipeResponse> recipeResponseList) {
+    RecipeAdapter(Context context, List<RecipeResponse> recipeResponseList) {
         mList = recipeResponseList;
         mContext = context;
     }
@@ -53,10 +67,21 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder,int i) {
+
         final RecipeResponse recipe = mList.get(i);
         viewHolder.recipeNameTV.setText(recipe.getName());
-
+        viewHolder.servingsTV.setText(String.format(mContext.getString(R.string.servings_text), recipe.getServings()));
+        viewHolder.recipeImageIV.setImageDrawable(mContext.getDrawable(imageID[i]));
+        //Setting on click listener on list item to open StepListActivity to Show steps
+        viewHolder.listItemRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext,StepListActivity.class);
+                intent.putExtra(StepListActivity.RECIPE_KEY,recipe);
+                mContext.startActivity(intent);
+            }
+        });
 
     }
 
