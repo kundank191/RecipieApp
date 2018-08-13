2d689d75b275134c1aff2dd0fc48ef4a409d13d1
Kundan <kundank191@gmail.com>
2018-08-13 12:23:31 +0530

    feat : update RecipeViewModel to be used by multiple activities

--- app/src/main/java/com/example/kunda/bakingapp/ui/RecipeViewModel.java
+++ app/src/main/java/com/example/kunda/bakingapp/ui/RecipeViewModel.java
@@ -8,12 +8,15 @@ import java.util.List;
 
 /**
  * Created by Kundan on 12-08-2018.
- *
+ * <p>
  * Stores the recipe list and helps it share across activity and fragments
  */
 public class RecipeViewModel extends ViewModel {
 
-    public RecipeViewModel(){
+    /**
+     * This Constructor will be used by the mainActivity to store the list of Recipe retrieved from the internet
+     */
+    RecipeViewModel() {
 
     }
 
@@ -25,13 +28,49 @@ public class RecipeViewModel extends ViewModel {
         this.mRecipeList = mRecipeList;
     }
 
-    public List<RecipeResponse.Ingredient> getIngredientList(int position){
-        return mRecipeList.get(position).getIngredients();
-    }
-
-    public List<RecipeResponse.Step> getStepList(int position){
-        return mRecipeList.get(position).getSteps();
-    }
-
     private List<RecipeResponse> mRecipeList = null;
+
+    /**
+     * Inner class in ViewModel which stores a particular recipe details it will be used bt StepList Activity
+     * I created this class to create a segregation for various activities which will be using this viewModel
+     */
+    public RecipeDetails getRecipeDetailsViewModel(RecipeResponse recipe) {
+        return new RecipeDetails(recipe);
+    }
+
+    public class RecipeDetails {
+        private RecipeResponse mRecipeDetails;
+
+        RecipeDetails(RecipeResponse recipeDetails) {
+            mRecipeDetails = recipeDetails;
+        }
+
+        public void setRecipeDetails(RecipeResponse recipeDetails) {
+            mRecipeDetails = recipeDetails;
+        }
+
+        public RecipeResponse getAllRecipeDetails() {
+            return mRecipeDetails;
+        }
+
+        public List<RecipeResponse.Step> getRecipeSteps() {
+            return mRecipeDetails.getSteps();
+        }
+
+        public List<RecipeResponse.Ingredient> getIngredientsList() {
+            return mRecipeDetails.getIngredients();
+        }
+
+        public String getRecipeName() {
+            return mRecipeDetails.getName();
+        }
+
+        public String getRecipeImageURL() {
+            return mRecipeDetails.getImage();
+        }
+
+        public int getServings() {
+            return mRecipeDetails.getServings();
+        }
+    }
 }
---
+++ app/src/main/java/com/example/kunda/bakingapp/ui/details/DetailsItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.kunda.bakingapp.ui.details;
+
+/**
+ * Created by Kundan on 13-08-2018.
+ */
+public class DetailsItemClickListener {
+}
